2625b76c2813f998795fbef709b1c93adb4e0011
Bryan Chug <bryan.chug@gmail.com>
2014-08-26 01:53:39 +0800

    added grails opts

---
+++ zsh/0002_opts.zsh
@@ -0,0 +1,2 @@
+export GRAILS_OPTS="-XX:MaxPermSize=1024m -Xms1024M -Xmx2560M -server -Xverify:none -XX:+UseParallelGC -noverify"
+#-javaagent:/home/bryanchug/.gvm/grails/2.3.9/lib/org.springframework/springloaded/jars/springloaded-1.2.0.RELEASE.jar"
