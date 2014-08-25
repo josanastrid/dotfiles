fd770f0fe548a90edcbe72ac70de068f25e64fb7
Josan Astrid <josanastrid@scarlet.local>
2014-08-26 01:40:08 +0800

    Updates

---
+++ zsh/0001_boxen.zsh
@@ -0,0 +1,8 @@
+# boxen
+[ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
+
+# gvm
+source ~/.gvm/bin/gvm-init.sh
+
+# autojump
+[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
